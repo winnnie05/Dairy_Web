aff16846071c85a40fc26236e121e566b9920119
AnB School 0031 <anbschool0031@AnBs-Mac-mini-100.local>
2024-06-24 20:52:00 +0700

    adding file and design

---
+++ js/introimg_injection.js
@@ -0,0 +1,15 @@
+const animalSticker = document.getElementById('animal-sticker')
+animalSticker.innerHTML = `
+    <div class="cell">
+        <img src="./assets/img/skicker1.png" alt="skicker1">
+    </div>
+    <div class="cell">
+        <img src="./assets/img/skicker2.png" alt="skicker2">
+    </div>
+    <div class="cell">
+        <img src="./assets/img/skicker3.png" alt="skicker3">
+    </div>
+    <div class="cell">
+        <img src="./assets/img/skicker4.png" alt="skicker4">
+    </div>
+`
---
+++ js/navbar_injection.js
@@ -0,0 +1,13 @@
+const navbar = document.getElementById('journaltime-navbar')
+navbar.innerHTML = `
+    <div class="journal-left-container">
+        <img src="./assets/img/Logo.png" alt="logo">
+    </div>
+
+    <div class="journal-right-conatainer">
+        <ul class="journal-sidebar">
+            <li><a href="#">Login</a></li>
+            <button class="go-wep-button"><li><a href="#">Go To Web App</a></li></button>
+        </ul>
+    </div>
+`
