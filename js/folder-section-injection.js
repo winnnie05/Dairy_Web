7de19e1a4ec609318e972a047411085fa3d211af
AnB School 0031 <anbschool0031@AnBs-Mac-mini-100.local>
2024-06-25 19:47:54 +0700

    design footer

---
+++ js/folder-section-injection.js
@@ -0,0 +1,62 @@
+const recentFolder = document.getElementById('folder-section-file')
+recentFolder.innerHTML = `
+ <div class="folder-container">
+        <div class="design-box">
+            <div class="dots">
+                <div class="dot"></div>
+                <div class="dot"></div>
+                <div class="dot"></div>
+            </div>
+        <div class="icon">
+            <img src="./assets/img/foldersticker.png" alt="Design Icon">
+        </div>
+        <h2 class="designh">Design</h2>
+        <p class="designp">Date: 21/06/2024</p>
+        </div>
+    </div>
+
+    <div class="folder-container">
+        <div class="design-box">
+            <div class="dots">
+                <div class="dot"></div>
+                <div class="dot"></div>
+                <div class="dot"></div>
+            </div>
+        <div class="icon">
+            <img src="./assets/img/foldersticker.png" alt="Design Icon">
+        </div>
+        <h2 class="designh">Design</h2>
+        <p class="designp">Date: 21/06/2024</p>
+        </div>
+    </div>
+
+    <div class="folder-container">
+        <div class="design-box">
+            <div class="dots">
+                <div class="dot"></div>
+                <div class="dot"></div>
+                <div class="dot"></div>
+            </div>
+        <div class="icon">
+            <img src="./assets/img/foldersticker.png" alt="Design Icon">
+        </div>
+        <h2 class="designh">Design</h2>
+        <p class="designp">Date: 21/06/2024</p>
+        </div>
+    </div>
+
+    <div class="folder-container">
+        <div class="design-box">
+            <div class="dots">
+                <div class="dot"></div>
+                <div class="dot"></div>
+                <div class="dot"></div>
+            </div>
+        <div class="icon">
+            <img src="./assets/img/foldersticker.png" alt="Design Icon">
+        </div>
+        <h2 class="designh">Design</h2>
+        <p class="designp">Date: 21/06/2024</p>
+        </div>
+    </div>
+`
